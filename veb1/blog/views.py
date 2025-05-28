752e3c31df257872342bb51898824e6973f715a2
Ангелина <angelinabarladina153@gmail.com>
2025-05-28 10:58:26 +0300

    Update views.py

--- veb1/blog/views.py
+++ veb1/blog/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Category, Order
+from .models import Product, Category, Order, abExam
 from django.db.models import Count, Avg
-from django.db.models import Q  # Import Q for complex lookups
+from django.db.models import Q  
 
 def home(request):
-    # 1. Популярные продукты (самые просматриваемые или покупаемые)
-    # Пример: Считаем количество заказов для каждого продукта (пока просто все продукты)
-    popular_products = Product.objects.all()  #Можно добавить логику подсчета просмотров/покупок, но пока просто список
-
-    # 2. Новые поступления (товары, добавленные недавно)
+    popular_products = Product.objects.all() 
     new_arrivals = Product.objects.order_by('-created_at')[:5]
-
-    # 3. Товары со скидкой (пример, пока нет поля "скидка", используем товары, цена которых ниже средней)
     average_price = Product.objects.aggregate(Avg('price'))['price__avg']
-    discounted_products = Product.objects.filter(price__lt=average_price)[:5] #Товары ниже средней цены
-
-    #4. Поиск
+    discounted_products = Product.objects.filter(price__lt=average_price)[:5]
+    # Поиск
     query = request.GET.get('q')
     results = []
     if query:
@@ -27,8 +20,8 @@ def home(request):
         'popular_products': popular_products,
         'new_arrivals': new_arrivals,
         'discounted_products': discounted_products,
-        'search_results': results,  # Передаем результаты поиска
-        'search_query': query,  # Передаем поисковый запрос
+        'search_results': results, 
+        'search_query': query, 
     }
     return render(request, 'blog/home.html', context)
 
@@ -36,3 +29,11 @@ def home(request):
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'blog/product_detail.html', {'product': product})
+
+def ab_exam_view(request):
+    exams = abExam.objects.filter(is_public=True)
+    context = {
+        'fio': 'Барладина Ангелина Александровна, группа 241-671',
+        'exams': exams,
+    }
+    return render(request, 'blog/ab_exam.html', context)
