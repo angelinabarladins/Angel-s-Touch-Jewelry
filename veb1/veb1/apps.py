ed64d309098e29c8d7b9a8c2588a42c91e541925
Ангелина <angelinabarladina153@gmail.com>
2025-05-28 10:38:52 +0300

    Update apps.py

--- veb1/veb1/apps.py
+++ veb1/veb1/apps.py
@@ -1,1 +1,12 @@
 
+from django.apps import AppConfig
+from django.conf import settings
+from django.contrib import admin
+
+
+class Veb1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'veb1'
+
+    def ready(self):
+        admin.site.site_header = settings.ADMIN_SITE_HEADER
